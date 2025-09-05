c2ea3d9aadafc6bac8c320092c953ed7fec0f081
Ed <ed@example.com>
2025-09-04 21:15:07 -0400

    Configure venv-selector to detect Miniconda environments

--- lua/python/venv-selector.lua
+++ lua/python/venv-selector.lua
@@ -9,6 +9,13 @@ return {
     { "<leader>mp", "<cmd>VenvSelect<cr>", desc = "Select Python Virtual Environment" },
   },
   opts = {
+    search = {
+      -- Miniconda environments in .miniconda3 directory
+      miniconda3 = {
+        command = "fd bin/python$ ~/.miniconda3/envs --full-path --color never",
+        type = "anaconda"
+      },
+    },
     options = {
       notify_user_on_venv_activation = true,
       activate_venv_in_terminal = true,
